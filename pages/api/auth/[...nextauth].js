7f475435c4234fb83599c2f69211fb855d18ef49
AriaFantom <priyanshudebnath348@gmail.com>
2022-01-14 15:45:28 +0530

    Pushed to check errors

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,16 +1,20 @@
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb";
 
 export default NextAuth({
-    providers: [
-        DiscordProvider({
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-
-
-        })
-    ],
-
-    secret: "hello",
-
-})
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    DiscordProvider({
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+    }),
+  ],
+  secret: "hello",
+  callbacks: {
+    async jwt({ user, account, token }) {
+      console.log(account);
+    },
+  },
+});
