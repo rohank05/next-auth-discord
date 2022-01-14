0e2b0cbd9363b3715d518b11c771fea6b6160d03
Rohan Kumar <rohan.shuvam@gmail.com>
2022-01-14 12:13:26 +0530

    and callback

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+
+export default NextAuth({
+    providers: [
+        DiscordProvider({
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+
+
+        })
+    ],
+
+    secret: "hello",
+
+})
